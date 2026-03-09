86978a76250576572371f944b974d030fd35895a
vivid-py <dobrostanandrij109+work@gmail.com>
2026-03-09 22:59:39 +0200

    Feat: add method to add a button to window

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ class App(ctk.CTk):
 		super().__init__()
 		self.geometry(f"{width}x{height}")
 		self.title(f"{title}")
-
+	def button(self,master: str, text: str, command: None):
+		btn = ctk.CTkButton(master= master, text=text, command = command)
+		return btn
+win.button(win, "Button", None).pack()
 win = App(400,400, "hz")
 
 win.mainloop()
