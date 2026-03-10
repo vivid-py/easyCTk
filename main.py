a1f198b0ff0d346c1b6b87a2c0675fdb10fcf6bc
vivid-py <dobrostanandrij109+work@gmail.com>
2026-03-10 11:20:17 +0200

    Feat 1: add grid layout support, and started to implement better button customization
    Implement grid/pack toggle

--- main.py
+++ main.py
@@ -5,22 +5,33 @@ class App(ctk.CTk):
 		self.resizable(resizable,resizable)
 		self.geometry(f"{width}x{height}")
 		self.title(f"{title}")
-	def button(self, text: str,command: None):
-		btn = ctk.CTkButton(self, text=text, command = command)
-		btn.pack()
+	def button(self, text: str="Button",command: str=None, color: str="red", hover: str="darkred", row: int=0, column: int=0, method: str="pack"):
+		btn = ctk.CTkButton(self, text=text, command = command, fg_color=color, hover_color=hover)
+		if method == "pack":
+			btn.pack()
+		elif method == "grid":
+			btn.grid(row=row, column=column, padx=10, pady=10)
 		return btn
-	def label(self, text: str):
-		label = ctk.CTkLabel(self, text=text)
+	def label(self, text: str = "Label"):
+		label = ctk.CTkLabel(self, text=text )
 		label.pack()
 		return label
-	def entry(self, placeholder_text: str):
+	def entry(self, placeholder_text: str="Entry"):
 		entry = ctk.CTkEntry(self,placeholder_text=placeholder_text)
 		entry.pack()
 		return entry
-	def checkbox(self):
-		checkbox = ctk.CTkCheckBox(self)
+	def checkbox(self, text: str="Checkbox"):
+		checkbox = ctk.CTkCheckBox(self, text=text)
 		checkbox.pack()
 		return checkbox
+	def run(self):
+		self.mainloop()
 
-win = App()
-win.mainloop()
+a = App()
+
+a.label("Label")
+a.button("Button")
+a.checkbox()
+a.entry()
+
+a.run()
